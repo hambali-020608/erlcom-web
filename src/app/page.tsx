73dfb62fce1ba7955ebc08fd50a4d4cb86f7f77f
hambali <subastianhambali@gmail.com>
2025-01-07 06:18:54 +0700

    menambahkan banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,71 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+                    <header className="bg-sky-500 text-white py-2 flex justify-between items-center px-4">
+                        <div className="flex items-center space-x-4">
+                            <span>Info</span>
+                            <span><i className="fas fa-phone-alt"></i> 081332428433</span>
+                            <span><i className="fas fa-phone-alt"></i> 081333106528</span>
+                        </div>
+                        <div className="flex space-x-4">
+                            <a href="#" className="text-white"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="text-white"><i className="fab fa-instagram"></i></a>
+                            <a href="#" className="text-white"><i className="fab fa-youtube"></i></a>
+                        </div>
+                    </header>
+                    <nav className="bg-transparent">
+                        <div className="container mx-auto flex justify-between items-center px-4">
+                            <div className="flex items-center space-x-4">
+                                <Image src="/images/logo/Erl.png" alt="Logo 1"  width={30} height={30}/>
+                                <Image src="/images/logo/lkp.png" alt="Logo 2" className="" width={30} height={30}/>
+                                
+                            </div>
+                            <ul className="flex space-x-6 text-gray-700">
+                                <li><a href="#" className="text-yellow-500">Home</a></li>
+                                <li><a href="#">Tentang Kami</a></li>
+                                <li><a href="#">Program</a></li>
+                                <li><a href="#">Gallery</a></li>
+                                <li><a href="#">FAQs</a></li>
+                                <li><a href="#">Testimoni</a></li>
+                                <li><a href="#">Alumni</a></li>
+                                <li><a href="#">Program Kampus</a></li>
+                                <li><a href="#">Karir</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <main>
+                        <div className="relative">
+                        <Image src='/images/banner/banner1.jpg' alt="erlcom banner 1" width={300} height={300} className="w-full"/>
+                            <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-50 py-4 flex justify-around items-center">
+                                <div className="text-center">
+                                    <i className="fas fa-award text-3xl"></i>
+                                    <p>ACHIEVEMENT</p>
+                                </div>
+                                <div className="text-center">
+                                    <i className="fas fa-user-graduate text-3xl"></i>
+                                    <p>CHARACTER</p>
+                                </div>
+                                <div className="text-center">
+                                    <i className="fas fa-globe text-3xl"></i>
+                                    <p>OVERSEAS</p>
+                                </div>
+                                <div className="text-center">
+                                    <i className="fas fa-concierge-bell text-3xl"></i>
+                                    <p>SERVICES</p>
+                                </div>
+                                <div className="text-center">
+                                    <i className="fas fa-lightbulb text-3xl"></i>
+                                    <p>LEADERSHIP</p>
+                                </div>
+                                <div className="text-center">
+                                    <i className="fab fa-whatsapp text-3xl text-green-500"></i>
+                                    <p>Hubungi Kami</p>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
   );
 }
